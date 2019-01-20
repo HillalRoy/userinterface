f609c93876d5e954be2326d964946e9e2c1f2086
Hillal Kumar Roy <Hillolroy85@gmail.com>
2019-01-20 18:32:07 +0530

    routes ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { WorksComponent } from './works/works.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    WorksComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
